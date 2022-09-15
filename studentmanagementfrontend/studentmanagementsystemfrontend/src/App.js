c5442215765e219e397f36a4d94987478e5a5c11
20IT068 <111736553+20IT068@users.noreply.github.com>
2022-09-15 20:23:23 +0530

    Added Footer

--- studentmanagementfrontend/studentmanagementsystemfrontend/src/App.js
+++ studentmanagementfrontend/studentmanagementsystemfrontend/src/App.js
@@ -1,13 +1,14 @@
+import Footer from './components/Footer';
 import NavigationBar from './components/NavigationBar';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
-
 function App() {
   return (
     <div className="App">
       <NavigationBar/>
       <Student/>
       <StudentList/>
+      <Footer/>
     </div>
   );
 }
