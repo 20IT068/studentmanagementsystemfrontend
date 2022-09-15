15fb930ff802130fbfbe11058922981f6aafd4f7
20IT068 <111736553+20IT068@users.noreply.github.com>
2022-09-15 20:34:21 +0530

    Added Edit Functionality

--- studentmanagementfrontend/studentmanagementsystemfrontend/src/App.js
+++ studentmanagementfrontend/studentmanagementsystemfrontend/src/App.js
@@ -3,7 +3,6 @@ import NavigationBar from './components/NavigationBar';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
-
 function App() {
   return (
     <div className="App">
@@ -12,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/student" />} />
         <Route path='student' element={<Student/>}/>
+        <Route path="student/:studentId" element={<Student />}/>
         <Route path='liststudents' element={<StudentList/>}/>
       </Routes>
       <Footer/>
--- studentmanagementfrontend/studentmanagementsystemfrontend/src/components/StudentList.js
+++ studentmanagementfrontend/studentmanagementsystemfrontend/src/components/StudentList.js
@@ -4,8 +4,6 @@ import { useState,useEffect } from "react";
 import axios from "axios";
 export default function StudentList() {
 const [student, setStudent] = useState([]);
-
-
   return (
     <div className="my-3">
       <Container>
@@ -21,7 +19,7 @@ const [student, setStudent] = useState([]);
               </tr>
             </thead>
             <tbody>
-            {/* <tr>
+            <tr>
                 <td>1</td>
                 <td>Mark</td>
                 <td>Otto</td>
@@ -50,20 +48,20 @@ const [student, setStudent] = useState([]);
                   &nbsp;&nbsp;
                   <button type="butoon" className="btn btn-danger">Delete</button>
                 </td>
-              </tr> */}
+              </tr>
               {student.map(student=> (
               <tr class="text-center">
                 <td>{student.id}</td>
                 <td>{student.name}</td>
                 <td>{student.address}</td>
                 <td>
-                  <button type="butoon" className="btn btn-primary" >Edit</button>
+                <Link to={"/student/"+student.id}> <button type="butoon" className="btn btn-primary" >Edit</button></Link>
                   &nbsp;&nbsp;
                   <button type="butoon" className="btn btn-danger">Delete</button>
                 </td>
               </tr>
               ))}
-
+         
             </tbody>
           </Table>
         </Card.Body>
